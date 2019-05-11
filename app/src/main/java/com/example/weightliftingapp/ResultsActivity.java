799dbbc1ee15efbcda4175e5cac19b40ef133da9
bjzuk <bjzukows@ualberta.ca>
2019-05-11 18:51:39 -0400

    finished the intent to pass data between the two activities, and the results activity is complete for now as a basic template

--- app/src/main/java/com/example/weightliftingapp/ResultsActivity.java
+++ app/src/main/java/com/example/weightliftingapp/ResultsActivity.java
@@ -3,6 +3,7 @@ package com.example.weightliftingapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ResultsActivity extends AppCompatActivity {
 
@@ -14,6 +15,15 @@ public class ResultsActivity extends AppCompatActivity {
         Intent intent = getIntent();
         double liftResponse = intent.getDoubleExtra(MainActivity.liftResponseAppId, 0);
         int repetitionResponse = intent.getIntExtra(MainActivity.repetitionResponseAppId, 0);
+
+        // run calculaton using RepMaxCalculator
+        // ...
+        // make sure to return the convert the value to a String before assigning it to the TextView below
+        // String oneRepMaxValue = RepMaxCalculator.Calculate().ToString();
+
+        // set response value to the TextView of this activity
+        TextView OneRepMaxResult = (TextView) findViewById(R.id.one_rep_max_result_value);
+        // OneRepMaxResult.setText(oneRepMaxValue);
 
     }
 }
